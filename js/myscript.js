8b9d7f832620f32e1b737f948112ebf17d41e5ff
UBI <wifor@p33.org>
2017-09-17 17:09:14 +0300

    test commit 3

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,25 @@
-var date = new Date();
-var time = date.getHours();
-if (time < 13 )
-    alert("Good Day!");
-else
-    alert("Good Evening");
+var country = {
+
+    name: "Швейцария",
+    languages: ["немецкий", "французский", "итальянский"],
+    capital:{
+
+        name: "Берн",
+        population: 126598
+    },
+    cities: [
+        { name: "Цюрих", population: 378884},
+        { name: "Женева", population: 188634},
+        { name: "Базель", population: 164937}
+    ]
+};
+
+// вывод всех элементов из country.languages
+document.write("<h3>Официальные языки Швейцарии</h3>");
+for(var i=0; i < country.languages.length; i++)
+    document.write(country.languages[i] + "<br/>");
+
+// вывод всех элементов из country.cities
+document.write("<h3>Города Швейцарии</h3>");
+for(var i=0; i < country.cities.length; i++)
+    document.write(country.cities[i].name + "<br/>");
